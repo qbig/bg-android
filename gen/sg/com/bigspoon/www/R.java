4e0111da23a4421f821ac357a0986fb28155f8cb
BapsMandava <bapaiah.mandava@gmail.com>
2014-05-19 15:28:25 +0800

    Dummy Commit

--- gen/sg/com/bigspoon/www/R.java
+++ gen/sg/com/bigspoon/www/R.java
@@ -27,15 +27,18 @@ public final class R {
         public static final int action_settings=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_login=0x7f030000;
+        public static final int activity_dummy=0x7f030000;
+        public static final int activity_login=0x7f030001;
     }
     public static final class menu {
-        public static final int login=0x7f070000;
+        public static final int dummy=0x7f070000;
+        public static final int login=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050003;
+        public static final int title_activity_dummy=0x7f050004;
         public static final int title_activity_login=0x7f050001;
     }
     public static final class style {
