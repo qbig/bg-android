98e3cba4065935de76496ec456e73a90f810dde4
Austin Bai <austinbai927@gmail.com>
2014-06-19 14:53:43 +0800

    Water Service Activity

--- BigSpoon/src/sg/com/bigspoon/www/activities/WaterServiceActivity.java
+++ BigSpoon/src/sg/com/bigspoon/www/activities/WaterServiceActivity.java
@@ -5,8 +5,11 @@ import sg.com.bigspoon.www.R.layout;
 import sg.com.bigspoon.www.adapters.WaterQuantityPickerAdapter;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class WaterServiceActivity extends Activity {
 	ActionBar actionBar;
@@ -17,6 +20,20 @@ public class WaterServiceActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_water_service);
+		Button btn1 = (Button) findViewById(R.id.ok);
+        Button btn2 = (Button) findViewById(R.id.cancle);
+        
+        btn2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	WaterServiceActivity.this.finish();
+            }
+        });
+        
+        btn1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	//
+            }
+        });
 
     }
 }
