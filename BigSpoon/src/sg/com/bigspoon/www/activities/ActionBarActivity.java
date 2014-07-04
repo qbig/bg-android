72a5491296a971d9e66e4ed7cc40859087fbc5f5
BapsMandava <bapaiah.mandava@gmail.com>
2014-07-05 02:25:43 +0800

    Dialog boxes

--- BigSpoon/src/sg/com/bigspoon/www/activities/ActionBarActivity.java
+++ BigSpoon/src/sg/com/bigspoon/www/activities/ActionBarActivity.java
@@ -5,14 +5,18 @@ import sg.com.bigspoon.www.R.id;
 import sg.com.bigspoon.www.R.layout;
 import sg.com.bigspoon.www.adapters.ActionBarMenuAdapter;
 import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.text.Editable;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.EditText;
 import android.widget.GridView;
 
 public class ActionBarActivity extends FragmentActivity {
@@ -31,11 +35,12 @@ public class ActionBarActivity extends FragmentActivity {
 
 		final Context ctx = getApplicationContext();
 		Resources res = ctx.getResources();
-
+		final EditText input = new EditText(this);
 		// String[] gv_options = {"Water","Waiter","Bill","Orders"};
 		// int[] gv_icons =
 		// {R.drawable.fb_1,R.drawable.fb_2,R.drawable.fb_3,R.drawable.fb_4};
 
+		final AlertDialog.Builder alert = new AlertDialog.Builder(this);
 		gridView = (GridView) findViewById(R.id.gv_action_menu);
 		// Create the Custom Adapter Object
 		ActionBarMenuAdapter actionBarMenuAdapter = new ActionBarMenuAdapter(
@@ -53,19 +58,51 @@ public class ActionBarActivity extends FragmentActivity {
 				switch (position) {
 				case 0:
 					i = new Intent(ctx, WaterServiceActivity.class);
+					i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+					startActivity(i);
 					break;
 				case 1:
-					// i = new Intent(ctx, StoreListActivity.class);
+					alert.setMessage("Please enter your table ID located on the BigSpoon table stand");
+
+					// Set an EditText view to get user input
+
+					alert.setView(input);
+
+					alert.setPositiveButton("Ok",new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,int whichButton) {
+									
+								}
+							}).setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,int whichButton) {
+								
+									
+								}
+							}).create().show();
 					break;
 				case 2:
-					// i = new Intent(ctx, CartActivity.class);
+					alert.setMessage("Would you like your bill?");
+
+					// Set an EditText view to get user input
+
+					alert.setPositiveButton("Ok",new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,int whichButton) {
+									// Editable value = input.getText();
+									// Do something with value!
+								}
+							}).setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,int whichButton) {
+									
+									
+								}
+							}).create().show();
 					break;
 				case 3:
 					i = new Intent(ctx, ItemsActivity.class);
+					i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+					startActivity(i);
 					break;
 				}
-				i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-				startActivity(i);
+				
 			}
 		});
 
