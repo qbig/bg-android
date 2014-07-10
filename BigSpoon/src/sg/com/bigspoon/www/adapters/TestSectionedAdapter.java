e5b4cae1eb9affa0f4efc21c33513f5ccadc1cf3
Austin Bai <austinbai927@gmail.com>
2014-07-10 17:23:26 +0800

    Remove lib, modifier complete

--- BigSpoon/src/sg/com/bigspoon/www/adapters/TestSectionedAdapter.java
+++ BigSpoon/src/sg/com/bigspoon/www/adapters/TestSectionedAdapter.java
@@ -6,10 +6,14 @@ import java.util.List;
 import sg.com.bigspoon.www.R;
 import android.content.Context;
 import android.text.Editable;
+import android.text.Layout;
 import android.text.TextWatcher;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
@@ -76,7 +80,7 @@ public class TestSectionedAdapter extends SectionedBaseAdapter {
     String[][] items = new String[][]{
     		{ "Garlic + Herbs", "Salted", "Unsalted", "Rum + Raisin"},
     		{ "Maple syrup"},
-    		{ "No","Yes (+$1)","test"},
+    		{ "No","Yes (+$1)","test","test1","test2"},
     		{ "Corn Beef Hash (+$2.5)", "Scrambled Eggs (+$2.5)", "Strawberry (+$1.5)", "Sauteed Mushrooms (+$2.5)","Banana (+$1.5)", "Chicken Chipolata (+$2.5)","Bacon (+$2.5)"}
     };
     private String[] type = {"count","count","radio","count"};
@@ -129,15 +133,17 @@ public class TestSectionedAdapter extends SectionedBaseAdapter {
     	
     	if (null == convertView) {
     		LayoutInflater inflator = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    		 switch(type[section]){
-    		 case "radio":
-    			 convertView = inflator.inflate(R.layout.activity_modifier_radiobutton, null);  
-    			 break;
-    		 case "count":
-    			 convertView =  inflator.inflate(R.layout.activity_modifier_counter, null);
-    			 break;
-    		 }
+    		 //switch(type[section]){
+    		 //case "radio":
+    			 convertView = inflator.inflate(R.layout.activity_modifier_addon, null);  
+    		//	 break;
+    		 //case "count":
+    		//	 convertView =  inflator.inflate(R.layout.activity_modifier_counter, null);
+    		//	 break;
+    		// }
     	}else{
+    		LayoutInflater inflator = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    		convertView = inflator.inflate(R.layout.activity_modifier_addon, null);  
         	viewHolder = (ViewHolder) convertView.getTag();
     	}
     	
@@ -147,11 +153,27 @@ public class TestSectionedAdapter extends SectionedBaseAdapter {
                 //convertView = inflator.inflate(R.layout.activity_modifier_radiobutton, null);            
                 //RadioButton rb = (RadioButton)layout.findViewById(R.id.radioButton1);             
         //}
+        	FrameLayout layoutholder = (FrameLayout)convertView.findViewById(R.id.frameLayout1);
+        	RadioButton radiobtn = new RadioButton(parent.getContext());
+        	final float scale = parent.getContext().getResources().getDisplayMetrics().density;
+        	int padding_5dp = (int) (5 * scale + 0.5f);
+        	int padding_25dp = (int) (25 * scale + 0.5f);
+        	int padding_37dp = (int) (37 * scale + 0.5f);
+        	int padding_35dp = (int) (35 * scale + 0.5f);
+        	FrameLayout.LayoutParams params =new FrameLayout.LayoutParams(padding_35dp, padding_35dp);
+        	params.gravity=Gravity.RIGHT;
+        	params.setMargins(0,0,padding_37dp,0);
+        	radiobtn.setLayoutParams(params);
+        	radiobtn.setPadding(padding_5dp,padding_5dp,padding_25dp,padding_5dp);
+        	radiobtn.setBackgroundResource(R.drawable.radiobackground);
+        	radiobtn.setButtonDrawable(android.R.color.transparent);
+        	layoutholder.addView(radiobtn);
+
         ((TextView) convertView.findViewById(R.id.text)).setText(items[section][position]); 
-        RadioButton rb = (RadioButton)convertView.findViewById(R.id.radioButton1); 
-        rb.setChecked(position == selectedPosition);
-        rb.setTag(position);
-        rb.setOnClickListener(new View.OnClickListener() {
+        //RadioButton rb = (RadioButton)convertView.findViewById(R.id.radioButton1); 
+        radiobtn.setChecked(position == selectedPosition);
+        radiobtn.setTag(position);
+        radiobtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 selectedPosition = (Integer)view.getTag();
@@ -167,13 +189,53 @@ public class TestSectionedAdapter extends SectionedBaseAdapter {
 
         if(type[section]=="count"){
     		//if (convertView == null) {
+        	FrameLayout layoutholder = (FrameLayout)convertView.findViewById(R.id.frameLayout1);
+        	ImageButton imagebtn1 = new ImageButton(parent.getContext());
+        	final float scale = parent.getContext().getResources().getDisplayMetrics().density;
+        	int padding_30dp = (int) (30 * scale + 0.5f);
+        	int padding_35dp = (int) (35 * scale + 0.5f);
+        	int width=padding_30dp;
+        	int height=padding_30dp;
+        	FrameLayout.LayoutParams params1 =new FrameLayout.LayoutParams(width, height,Gravity.CENTER|Gravity.LEFT);
+        	//params1.gravity=Gravity.LEFT;
+        	imagebtn1.setLayoutParams(params1);
+        	//imagebtn1.setPadding(padding_5dp,padding_5dp,padding_25dp,padding_5dp);
+        	imagebtn1.setBackgroundResource(R.drawable.btn_minus_with_circle);
+        	layoutholder.addView(imagebtn1);
+        	
+        	
+        	ImageButton imagebtn2 = new ImageButton(parent.getContext());
+        	FrameLayout.LayoutParams params2 =new FrameLayout.LayoutParams(width, height,Gravity.CENTER|Gravity.RIGHT);
+        	//params2.gravity=Gravity.RIGHT;
+        	imagebtn2.setLayoutParams(params2);
+        	//imagebtn1.setPadding(padding_5dp,padding_5dp,padding_25dp,padding_5dp);
+        	imagebtn2.setBackgroundResource(R.drawable.add_btn_with_circle);
+        	layoutholder.addView(imagebtn2);
+        	
+        	
+        	TextView textview3 = new TextView(parent.getContext());
+        	int width3=padding_35dp;
+        	int height3=padding_35dp;
+        	FrameLayout.LayoutParams params3 =new FrameLayout.LayoutParams(width3, height3);
+        	params3.gravity=Gravity.CENTER;
+        	textview3.setLayoutParams(params3);
+        	textview3.setBackgroundResource(R.drawable.circle_for_text);
+        	textview3.setText("0");
+        	textview3.setTextColor( parent.getContext().getResources().getColor(R.color.black));
+        	textview3.setTextSize(19);
+        	textview3.setGravity(Gravity.CENTER);
+        	textview3.setId(R.id.textPicker);
+        	layoutholder.addView(textview3);
+        	
+        	
+        	
     	    viewHolder = new ViewHolder();
             //LayoutInflater inflator = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             //convertView =  inflator.inflate(R.layout.activity_modifier_counter, null);
             viewHolder.text=((TextView) convertView.findViewById(R.id.text));
-            viewHolder.decre=(ImageButton)convertView.findViewById(R.id.imageButton1);
-            viewHolder.incre=(ImageButton)convertView.findViewById(R.id.imageButton2);
-            viewHolder.textView=(TextView)convertView.findViewById(R.id.text2);
+            viewHolder.decre=imagebtn1;
+            viewHolder.incre=imagebtn2;
+            viewHolder.textView=textview3;
             final TextView temp= viewHolder.textView;
 
             //add listener for the text change in the textview
@@ -215,7 +277,7 @@ public class TestSectionedAdapter extends SectionedBaseAdapter {
 			}
         });
             convertView.setTag(viewHolder);
-            convertView.setTag(R.id.text2, viewHolder.textView);
+            convertView.setTag(R.id.textPicker, viewHolder.textView);
            // }else {
             //    viewHolder = (ViewHolder) convertView.getTag();
            // }
