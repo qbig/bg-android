6011dcf7f5d39232c6b8bb71ec4513ba2ccb006b
Qiao Liang <qiaoliang89@gmail.com>
2014-07-17 16:44:44 +0800

    fix dummyDish related but

--- BigSpoon/src/sg/com/bigspoon/www/adapters/MenuListViewAdapter.java
+++ BigSpoon/src/sg/com/bigspoon/www/adapters/MenuListViewAdapter.java
@@ -160,6 +160,9 @@ public class MenuListViewAdapter extends BaseAdapter {
 			if (currentDish.isDummyDish()){
 				photoViewHolder.imageButton.setVisibility(View.GONE);
 				photoViewHolder.textItemPrice.setVisibility(View.GONE);
+			} else {
+				photoViewHolder.imageButton.setVisibility(View.VISIBLE);
+				photoViewHolder.textItemPrice.setVisibility(View.VISIBLE);
 			}
 			
 			return convertView;
@@ -188,6 +191,9 @@ public class MenuListViewAdapter extends BaseAdapter {
 			if (currentDish.isDummyDish()){
 				textViewHolder.imageButton.setVisibility(View.GONE);
 				textViewHolder.textItemPrice.setVisibility(View.GONE);
+			} else {
+				textViewHolder.imageButton.setVisibility(View.VISIBLE);
+				textViewHolder.textItemPrice.setVisibility(View.VISIBLE);
 			}
 			
 			return convertView;
