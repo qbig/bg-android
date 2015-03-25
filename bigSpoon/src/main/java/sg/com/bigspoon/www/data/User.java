4f5abc6c5153b1d79567c96e25fd83affb9a5a1c
Qiao Liang <qiaoliang89@gmail.com>
2015-03-25 10:48:52 +0800

    set location check always true

--- bigSpoon/src/main/java/sg/com/bigspoon/www/data/User.java
+++ bigSpoon/src/main/java/sg/com/bigspoon/www/data/User.java
@@ -21,9 +21,7 @@ import java.util.HashMap;
 import java.util.List;
 
 import static sg.com.bigspoon.www.data.Constants.LOGIN_INFO_AUTHTOKEN;
-import static sg.com.bigspoon.www.data.Constants.LOGIN_INFO_EMAIL;
 import static sg.com.bigspoon.www.data.Constants.NOTIF_ORDER_UPDATE;
-import static sg.com.bigspoon.www.data.Constants.NOTIF_TO_START_LOCATION_SERVICE;
 import static sg.com.bigspoon.www.data.Constants.ORDER_URL;
 import static sg.com.bigspoon.www.data.Constants.PREFS_NAME;
 import static sg.com.bigspoon.www.data.Constants.REQUEST_URL;
@@ -55,32 +53,33 @@ public class User {
 	}
 	
 	public Boolean checkLocation() {
-		final String email = loginPrefs.getString(LOGIN_INFO_EMAIL, null);  
-		if ( email!= null && email.equals("jay.tjk@gmail.com")){
-			return true;
-		}
-		
-		if (currentLocation == null) {
-			Intent intent = new Intent(NOTIF_TO_START_LOCATION_SERVICE);
-			LocalBroadcastManager.getInstance(mContext).sendBroadcast(intent);
-			return false;
-		}
-		Boolean checkLocationPass = false;
-		double accuracy = 0;
-		final Location locationOfCurrentOutlet = new Location("Current Outlet");
-		locationOfCurrentOutlet.setLatitude(currentOutlet.lat);
-		locationOfCurrentOutlet.setLongitude(currentOutlet.lng);
-		double distance = currentLocation.distanceTo(locationOfCurrentOutlet);
-		if (currentLocation.hasAccuracy()) {
-			accuracy = currentLocation.getAccuracy();
-		}
-		if (distance <= accuracy + currentOutlet.locationThreshold) {
-			checkLocationPass = true;
-		} else {
-			checkLocationPass = false;
-		}
-
-		return checkLocationPass;
+        return true;
+//		final String email = loginPrefs.getString(LOGIN_INFO_EMAIL, null);
+//		if ( email!= null && email.equals("jay.tjk@gmail.com")){
+//			return true;
+//		}
+//
+//		if (currentLocation == null) {
+//			Intent intent = new Intent(NOTIF_TO_START_LOCATION_SERVICE);
+//			LocalBroadcastManager.getInstance(mContext).sendBroadcast(intent);
+//			return false;
+//		}
+//		Boolean checkLocationPass = false;
+//		double accuracy = 0;
+//		final Location locationOfCurrentOutlet = new Location("Current Outlet");
+//		locationOfCurrentOutlet.setLatitude(currentOutlet.lat);
+//		locationOfCurrentOutlet.setLongitude(currentOutlet.lng);
+//		double distance = currentLocation.distanceTo(locationOfCurrentOutlet);
+//		if (currentLocation.hasAccuracy()) {
+//			accuracy = currentLocation.getAccuracy();
+//		}
+//		if (distance <= accuracy + currentOutlet.locationThreshold) {
+//			checkLocationPass = true;
+//		} else {
+//			checkLocationPass = false;
+//		}
+//
+//		return checkLocationPass;
 	}
 
 	static public User getInstance(Context context) {
