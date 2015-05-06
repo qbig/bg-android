973842494bfe9ac3c4e1866b09d06618ec5c9b54
Qiao Liang <qiaoliang89@gmail.com>
2015-05-07 04:37:30 +0800

    added delay for dismissing reminder popup

--- bigSpoon/src/main/java/sg/com/bigspoon/www/activities/ImageDialog.java
+++ bigSpoon/src/main/java/sg/com/bigspoon/www/activities/ImageDialog.java
@@ -2,6 +2,7 @@ package sg.com.bigspoon.www.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
@@ -14,18 +15,23 @@ import sg.com.bigspoon.www.R;
 public class ImageDialog extends Activity {
 
     private ImageView mDialog;
-
+    private android.os.Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.image_dialog_layout);
-
+        handler = new Handler();
         mDialog = (ImageView)findViewById(R.id.popup_image);
         mDialog.setClickable(true);
         mDialog.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                ImageDialog.this.handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        finish();
+                    }
+                }, 1000);
             }
         });
 
