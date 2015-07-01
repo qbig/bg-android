5afa71080689e1263cf24b4e9eb6ccb5daa312de
Qiao Liang <qiaoliang89@gmail.com>
2015-07-01 20:11:47 +0800

    update build file with crosswalk webview

--- bigSpoon/src/main/java/sg/com/bigspoon/www/data/BigSpoon.java
+++ bigSpoon/src/main/java/sg/com/bigspoon/www/data/BigSpoon.java
@@ -40,160 +40,160 @@ import static sg.com.bigspoon.www.data.Constants.TABLE_ID;
 import static sg.com.bigspoon.www.data.Constants.TUTORIAL_SET;
 
 public class BigSpoon extends Application implements Foreground.Listener {
-	final Handler mHandler = new Handler();
-	public boolean onStart = true;
-	private MixpanelAPI mMixpanel;
-	private BroadcastReceiver mLocationUpdateReceiver = new BroadcastReceiver() {
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			final Location newLocation = intent.getParcelableExtra(NOTIF_LOCATION_KEY);
-			Location currentLoc = User.getInstance(getApplicationContext()).currentLocation;
-			if (currentLoc == null || (currentLoc.hasAccuracy() && newLocation.getAccuracy() < currentLoc.getAccuracy())){
-				User.getInstance(getApplicationContext()).currentLocation = newLocation;
-			}
-		}
-	};
+    final Handler mHandler = new Handler();
+    public boolean onStart = true;
+    private MixpanelAPI mMixpanel;
+    private BroadcastReceiver mLocationUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final Location newLocation = intent.getParcelableExtra(NOTIF_LOCATION_KEY);
+            Location currentLoc = User.getInstance(getApplicationContext()).currentLocation;
+            if (currentLoc == null || (currentLoc.hasAccuracy() && newLocation.getAccuracy() < currentLoc.getAccuracy())) {
+                User.getInstance(getApplicationContext()).currentLocation = newLocation;
+            }
+        }
+    };
 
-	private BroadcastReceiver mLocationStartServiceReceiver = new BroadcastReceiver() {
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			BigSpoon.this.startService(new Intent(BigSpoon.this, BGLocationService.class));
-		}
-	};
+    private BroadcastReceiver mLocationStartServiceReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            BigSpoon.this.startService(new Intent(BigSpoon.this, BGLocationService.class));
+        }
+    };
 
-	private final BroadcastReceiver lftBroadcastReceiver = new BroadcastReceiver() {
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			final LocationInfo locationInfo = (LocationInfo) intent
-					.getSerializableExtra(LocationLibraryConstants.LOCATION_BROADCAST_EXTRA_LOCATIONINFO);
-			if (locationInfo.anyLocationDataReceived()) {
-				Location currentLoc = User.getInstance(getApplicationContext()).currentLocation;
-				if (currentLoc == null || (currentLoc.hasAccuracy() &&
-						locationInfo.lastAccuracy < currentLoc.getAccuracy() && locationInfo.lastLocationUpdateTimestamp > currentLoc.getTime())){
-					currentLoc = new Location("toUpdate");
-					currentLoc.setLatitude(locationInfo.lastLat);
-					currentLoc.setLongitude(locationInfo.lastLong);
-					User.getInstance(getApplicationContext()).currentLocation = currentLoc;
-				}
-			}
-		}
-	};
+    private final BroadcastReceiver lftBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final LocationInfo locationInfo = (LocationInfo) intent
+                    .getSerializableExtra(LocationLibraryConstants.LOCATION_BROADCAST_EXTRA_LOCATIONINFO);
+            if (locationInfo.anyLocationDataReceived()) {
+                Location currentLoc = User.getInstance(getApplicationContext()).currentLocation;
+                if (currentLoc == null || (currentLoc.hasAccuracy() &&
+                        locationInfo.lastAccuracy < currentLoc.getAccuracy() && locationInfo.lastLocationUpdateTimestamp > currentLoc.getTime())) {
+                    currentLoc = new Location("toUpdate");
+                    currentLoc.setLatitude(locationInfo.lastLat);
+                    currentLoc.setLongitude(locationInfo.lastLong);
+                    User.getInstance(getApplicationContext()).currentLocation = currentLoc;
+                }
+            }
+        }
+    };
 
-	@Override
-	public void onCreate() {
-		super.onCreate();
+    @Override
+    public void onCreate() {
+        super.onCreate();
         Fabric.with(this, new Crashlytics());
-		try {
-			LocationLibrary.initialiseLibrary(getBaseContext(), "sg.com.bigspoon.www");
-		} catch (UnsupportedOperationException ex) {
-			Crashlytics.logException(ex);
-		}
-		
-		Foreground.get(this).addListener(this);
-		LocalBroadcastManager.getInstance(this).registerReceiver(mLocationUpdateReceiver,
-				new IntentFilter(NOTIF_LOCATION_UPDATED));
-		LocalBroadcastManager.getInstance(this).registerReceiver(mLocationStartServiceReceiver,
-				new IntentFilter(NOTIF_TO_START_LOCATION_SERVICE));
-		LocalBroadcastManager.getInstance(this).registerReceiver(lftBroadcastReceiver, new IntentFilter(LocationLibraryConstants.getLocationChangedPeriodicBroadcastAction()));
-		BugSenseHandler.initAndStartSession(this, "625f7944");
-		mMixpanel = MixpanelAPI.getInstance(this, MIXPANEL_TOKEN);
-		mMixpanel.identify(mMixpanel.getDistinctId());
-		mMixpanel.getPeople().identify(mMixpanel.getDistinctId());
+        try {
+            LocationLibrary.initialiseLibrary(getBaseContext(), "sg.com.bigspoon.www");
+        } catch (UnsupportedOperationException ex) {
+            Crashlytics.logException(ex);
+        }
 
-		User.getInstance(this).mMixpanel = this.mMixpanel;
-		final SharedPreferences pref = getSharedPreferences(PREFS_NAME, 0);
-		if (pref.contains(LOGIN_INFO_EMAIL)) {
-			final String email = pref.getString(LOGIN_INFO_EMAIL, null);
-			if (email != null) {
-				JSONObject props = new JSONObject();
-				try {
-					props.put("user", email);
-					mMixpanel.track("Usage starts", props);
-				} catch (JSONException e) {
-					e.printStackTrace();
-				}
-			}
-		}
+        Foreground.get(this).addListener(this);
+        LocalBroadcastManager.getInstance(this).registerReceiver(mLocationUpdateReceiver,
+                new IntentFilter(NOTIF_LOCATION_UPDATED));
+        LocalBroadcastManager.getInstance(this).registerReceiver(mLocationStartServiceReceiver,
+                new IntentFilter(NOTIF_TO_START_LOCATION_SERVICE));
+        LocalBroadcastManager.getInstance(this).registerReceiver(lftBroadcastReceiver, new IntentFilter(LocationLibraryConstants.getLocationChangedPeriodicBroadcastAction()));
+        BugSenseHandler.initAndStartSession(this, "625f7944");
+        mMixpanel = MixpanelAPI.getInstance(this, MIXPANEL_TOKEN);
+        mMixpanel.identify(mMixpanel.getDistinctId());
+        mMixpanel.getPeople().identify(mMixpanel.getDistinctId());
+
+        User.getInstance(this).mMixpanel = this.mMixpanel;
+        final SharedPreferences pref = getSharedPreferences(PREFS_NAME, 0);
+        if (pref.contains(LOGIN_INFO_EMAIL)) {
+            final String email = pref.getString(LOGIN_INFO_EMAIL, null);
+            if (email != null) {
+                JSONObject props = new JSONObject();
+                try {
+                    props.put("user", email);
+                    mMixpanel.track("Usage starts", props);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
                         .setDefaultFontPath("fonts/ProximaNovaSemibold.otf")
                         .setFontAttrId(R.attr.fontPath)
                         .build()
         );
-	}
+    }
 
-	@Override
-	public void onTerminate() {
-		super.onTerminate();
-		Foreground.get(this).removeListener(this);
-		LocalBroadcastManager.getInstance(this).unregisterReceiver(mLocationUpdateReceiver);
-		LocalBroadcastManager.getInstance(this).unregisterReceiver(mLocationStartServiceReceiver);
-		BugSenseHandler.closeSession(this);
-		mMixpanel.flush();
-	}
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        Foreground.get(this).removeListener(this);
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mLocationUpdateReceiver);
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mLocationStartServiceReceiver);
+        BugSenseHandler.closeSession(this);
+        mMixpanel.flush();
+    }
 
-	// Foreground Callback
-	@Override
-	public void onBecameForeground() {
-		//User.getInstance(getApplicationContext()).updateOrder();
-		final User user = User.getInstance(this);
+    // Foreground Callback
+    @Override
+    public void onBecameForeground() {
+        //User.getInstance(getApplicationContext()).updateOrder();
+        final User user = User.getInstance(this);
         user.tableId = getSharedPreferences(PREFS_NAME, 0).getInt(TABLE_ID, -1);
-		SocketIOManager.getInstance(this).setupSocketIOConnection();
-		this.startService(new Intent(this, BGLocationService.class));
-		checkLocationEnabledIfTutorialHasShown();
-		LocationLibrary.startAlarmAndListener(this);
-		final int outletId = getSharedPreferences(PREFS_NAME, 0).getInt(OUTLET_ID, -1);
-		final int delay = onStart ? 5000 : 0;
-		if (outletId != -1){
-			// fix on app start, popup gets dismissed.
-			new Handler().postDelayed(new Runnable() {
-				@Override
-				public void run() {
-					if (user.shouldGoToOutlet) {
-						Intent intent = new Intent(BigSpoon.this, OutletListActivity.class);
-						intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-						BigSpoon.this.startActivity(intent);
-					}
-				}
-			}, delay);
-		}
-		onStart = false; // only delay on app start.
-	}
+        SocketIOManager.getInstance(this).setupSocketIOConnection();
+        this.startService(new Intent(this, BGLocationService.class));
+        checkLocationEnabledIfTutorialHasShown();
+        LocationLibrary.startAlarmAndListener(this);
+        final int outletId = getSharedPreferences(PREFS_NAME, 0).getInt(OUTLET_ID, -1);
+        final int delay = onStart ? 5000 : 0;
+        if (outletId != -1) {
+            // fix on app start, popup gets dismissed.
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if (user.shouldGoToOutlet) {
+                        Intent intent = new Intent(BigSpoon.this, OutletListActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        BigSpoon.this.startActivity(intent);
+                    }
+                }
+            }, delay);
+        }
+        onStart = false; // only delay on app start.
+    }
 
-	// Foreground Callback
-	@Override
-	public void onBecameBackground() {
-		this.stopService(new Intent(this, BGLocationService.class));
-		SocketIOManager.getInstance(this).disconnect();
-		LocationLibrary.stopAlarmAndListener(this);
-		User.getInstance(this).shouldGoToOutlet = true;
-		User.getInstance(this).prevActiveTime= System.currentTimeMillis();
-	}
+    // Foreground Callback
+    @Override
+    public void onBecameBackground() {
+        this.stopService(new Intent(this, BGLocationService.class));
+        SocketIOManager.getInstance(this).disconnect();
+        LocationLibrary.stopAlarmAndListener(this);
+        User.getInstance(this).shouldGoToOutlet = true;
+        User.getInstance(this).prevActiveTime = System.currentTimeMillis();
+    }
 
-	public void checkLocationEnabledByForce() {
-		checkLocationEnabled(true);
-	}
+    public void checkLocationEnabledByForce() {
+        checkLocationEnabled(true);
+    }
 
-	public void checkLocationEnabledIfTutorialHasShown() {
-		checkLocationEnabled(false);
-	}
+    public void checkLocationEnabledIfTutorialHasShown() {
+        checkLocationEnabled(false);
+    }
 
-	private void checkLocationEnabled(boolean force) {
-		final SharedPreferences loginPreferences = getSharedPreferences(PREFS_NAME, 0);
-		final SharedPreferences.Editor loginEditor = loginPreferences.edit();
+    private void checkLocationEnabled(boolean force) {
+        final SharedPreferences loginPreferences = getSharedPreferences(PREFS_NAME, 0);
+        final SharedPreferences.Editor loginEditor = loginPreferences.edit();
 
-		final boolean hasShownTutorial = loginPreferences.getBoolean(TUTORIAL_SET, false);
-		if (hasShownTutorial || force) {
-			final LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-			if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-				Toast.makeText(this, R.string.please_turn_on_gps, Toast.LENGTH_LONG).show();
-				Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-				myIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-				startActivity(myIntent);
-			}
+        final boolean hasShownTutorial = loginPreferences.getBoolean(TUTORIAL_SET, false);
+        if (hasShownTutorial || force) {
+            final LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                Toast.makeText(this, R.string.please_turn_on_gps, Toast.LENGTH_LONG).show();
+                Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                myIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(myIntent);
+            }
 
-			loginEditor.putBoolean(TUTORIAL_SET, true);
-			loginEditor.commit();
-		}
-	}
+            loginEditor.putBoolean(TUTORIAL_SET, true);
+            loginEditor.commit();
+        }
+    }
 }
